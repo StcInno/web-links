1d778729d2800b9fd3f468d0914555270bd43312
Vallek <vforval86@gmail.com>
2021-03-07 16:09:11 +0300

    фикс лишнего класса но на самом деле нет

--- js/filter.js
+++ js/filter.js
@@ -8,6 +8,77 @@ function submit(evt) {
 	evt.preventDefault();
 }
 
+function filter(evt) {
+	evt.preventDefault();
+	let inputValue = input.value.toUpperCase();	
+
+	listItems.forEach(
+		function getMatch(info) {
+			let listLinks = info.querySelectorAll('a');
+			listLinks.forEach(
+				function allLinks(item) {
+					let linkText = item.innerHTML.toUpperCase();
+					let firstItem = main.querySelector('article:not(.hide)');
+					if (linkText.includes(inputValue)) {
+						info.classList.add('show');
+						info.classList.remove('hide');
+						if (firstItem != null) {
+							firstItem.classList.add('first-item');	
+						}
+						else {
+							return;
+						}
+					}
+					else {
+						info.classList.add('hide');
+						info.classList.remove('show');
+						info.parentElement.classList.remove('first-item');
+					}
+				}
+			);
+		}
+	);
+	
+	// let heading = main.querySelectorAll('h2');
+	// heading.forEach(
+	// 	function allHeads(item) {
+	// 		let headingText = item.innerHTML.toUpperCase();
+	// 		let thisArticle = item.parentElement;
+	// 		console.log(thisArticle);
+	// 		if (headingText.includes(inputValue)) {
+	// 			thisArticle.classList.add('show');
+	// 			thisArticle.classList.remove('hide');	
+	// 		}
+	// 		else {
+	// 			thisArticle.classList.add('hide');
+	// 			thisArticle.classList.remove('show');
+	// 		}
+	// 	}
+	// );
+
+	article.forEach(
+		function hideArticles(item) {
+			if (! item.querySelector('.show')) {
+				item.classList.add('hide');
+			}
+			else {
+				item.classList.remove('hide');
+			}
+		}
+	);
+	section.forEach(
+		function hideSections(item) {
+			if (! item.querySelector('.show')) {
+				item.classList.add('hide');
+			}
+			else {
+				item.classList.remove('hide');
+			}
+		}
+	);
+}
+
+
 function autoReset() {
 	if (input.value == null, input.value == "") {
 		let allHide = main.querySelectorAll('.hide');
@@ -34,73 +105,6 @@ function autoReset() {
 	else {
 		return;
 	}			
-}
-
-function filter(evt) {
-	evt.preventDefault();
-	let inputValue = input.value.toUpperCase();	
-
-	listItems.forEach(
-		function getMatch(info) {
-			let listLinks = info.querySelectorAll('a');
-			listLinks.forEach(
-				function allLinks(item) {
-					let linkText = item.innerHTML.toUpperCase();
-					let firstItem = main.querySelector('article:not(.hide)');
-					if (linkText.includes(inputValue)) {
-						info.classList.add('show');
-						info.classList.remove('hide');
-						firstItem.classList.remove('first-item');		
-					}
-					else {
-						info.classList.add('hide');
-						info.classList.remove('show');
-						firstItem.classList.add('first-item');
-					}
-				}
-			);
-		}
-	);
-	
-	// let heading = main.querySelectorAll('h2');
-	// heading.forEach(
-	// 	function allHeads(item) {
-	// 		let headingText = item.innerHTML.toUpperCase();
-	// 		let thisArticle = item.parentElement.parentElement;
-	// 		let firstItem = main.querySelector('article:not(.hide)');
-	// 		if (headingText.includes(inputValue)) {
-	// 			thisArticle.classList.add('show');
-	// 			thisArticle.classList.remove('hide');	
-	// 			firstItem.classList.remove('first-item');	
-	// 		}
-	// 		else {
-	// 			thisArticle.classList.add('hide');
-	// 			thisArticle.classList.remove('show');
-	// 			firstItem.classList.add('first-item');
-	// 		}
-	// 	}
-	// );
-
-article.forEach(
-	function hideArticles(item) {
-		if (! item.querySelector('.show')) {
-			item.classList.add('hide');
-		}
-		else {
-			item.classList.remove('hide');
-		}
-	}
-);
-section.forEach(
-	function hideSections(item) {
-		if (! item.querySelector('.show')) {
-			item.classList.add('hide');
-		}
-		else {
-			item.classList.remove('hide');
-		}
-	}
-);
 }
 
 let form = document.querySelector('.search-form');
