57380e6e1d0fd84adc0978cbd3d7467f528edf60
Vallek <vforval86@gmail.com>
2021-03-07 10:59:29 +0300

    добавляет отступ у первого блока и убирает лишние классы фикс ошибок

--- js/filter.js
+++ js/filter.js
@@ -8,6 +8,34 @@ function submit(evt) {
 	evt.preventDefault();
 }
 
+function autoReset() {
+	if (input.value == null, input.value == "") {
+		let allHide = main.querySelectorAll('.hide');
+		let allShow = main.querySelectorAll('.show');
+		let firstItem = main.querySelectorAll('article.first-item');
+		allHide.forEach(
+			function showAll(items) {
+				items.classList.remove('hide');
+				items.classList.remove('show');
+			}
+		);
+		allShow.forEach(
+			function showAll(items) {
+				items.classList.remove('hide');
+				items.classList.remove('show');
+			}
+		);
+		firstItem.forEach(
+			function removeFirst(item) {
+				item.classList.remove('first-item');	
+			}
+		);
+	}
+	else {
+		return;
+	}			
+}
+
 function filter(evt) {
 	evt.preventDefault();
 	let inputValue = input.value.toUpperCase();	
@@ -34,51 +62,50 @@ function filter(evt) {
 		}
 	);
 	
-	article.forEach(
-		function hideArticles(item) {
-			if (! item.querySelector('.show')) {
-				item.classList.add('hide');
-			}
-			else {
-				item.classList.remove('hide');
-			}
-		}
-	);
-	section.forEach(
-		function hideSections(item) {
-			if (! item.querySelector('.show')) {
-				item.classList.add('hide');
-			}
-			else {
-				item.classList.remove('hide');
-			}
-		}
-	);
-}
+	// let heading = main.querySelectorAll('h2');
+	// heading.forEach(
+	// 	function allHeads(item) {
+	// 		let headingText = item.innerHTML.toUpperCase();
+	// 		let thisArticle = item.parentElement.parentElement;
+	// 		let firstItem = main.querySelector('article:not(.hide)');
+	// 		if (headingText.includes(inputValue)) {
+	// 			thisArticle.classList.add('show');
+	// 			thisArticle.classList.remove('hide');	
+	// 			firstItem.classList.remove('first-item');	
+	// 		}
+	// 		else {
+	// 			thisArticle.classList.add('hide');
+	// 			thisArticle.classList.remove('show');
+	// 			firstItem.classList.add('first-item');
+	// 		}
+	// 	}
+	// );
 
-function autoReset() {
-			if (input.value == null, input.value == "") {
-				let allHide = main.querySelectorAll('.hide');
-				let allShow = main.querySelectorAll('.show');
-				allHide.forEach(
-					function showAll(items) {
-						items.classList.remove('hide');
-						items.classList.remove('show');
-					}
-				);
-				allShow.forEach(
-					function showAll(items) {
-						items.classList.remove('hide');
-						items.classList.remove('show');
-					}
-				);
-			}
-			else {
-				return;
-			}			
+article.forEach(
+	function hideArticles(item) {
+		if (! item.querySelector('.show')) {
+			item.classList.add('hide');
+		}
+		else {
+			item.classList.remove('hide');
+		}
+	}
+);
+section.forEach(
+	function hideSections(item) {
+		if (! item.querySelector('.show')) {
+			item.classList.add('hide');
+		}
+		else {
+			item.classList.remove('hide');
+		}
+	}
+);
 }
 
 let form = document.querySelector('.search-form');
+
+form.addEventListener('keyup', autoReset);
 
 form.addEventListener('keyup', filter);
 
